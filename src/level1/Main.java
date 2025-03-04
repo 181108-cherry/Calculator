b4042fc12205b5f59e1ac0c3e228393dc138f6b0
강다연 <dysy9799@naver.com>
2025-03-04 16:03:50 +0900

    switch문 사용하여 연산 진행

--- src/level1/Main.java
+++ src/level1/Main.java
@@ -16,6 +16,26 @@ public class Main {
         System.out.println("사칙연산 기호를 입력하세요(+, -, *, /) : ");
         char operation = scanner.next().charAt(0);
 
+        int result = 0;
+
+        switch (operation) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+
+            default:
+                System.out.println("값이 일치하지 않습니다.");
+        }
+
 
     }
 }
