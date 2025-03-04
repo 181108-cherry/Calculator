97f04ab389a20ae330f09497e7b9d1bf63f64e8c
강다연 <dysy9799@naver.com>
2025-03-04 15:50:47 +0900

    사용자에게 숫자와 사칙연산 기호 입력받기

--- src/level1/Main.java
+++ src/level1/Main.java
@@ -1,4 +1,21 @@
 package level1;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("첫번째 숫자를 입력하세요 : ");
+        int a = scanner.nextInt();
+
+        System.out.println("두번째 숫자를 입력하세요 : ");
+        int b = scanner.nextInt();
+
+        System.out.println("사칙연산 기호를 입력하세요(+, -, *, /) : ");
+        char operation = scanner.next().charAt(0);
+
+
+    }
 }
