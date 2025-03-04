f12a656916f656173fc94612c0dfbe1b354085f1
강다연 <dysy9799@naver.com>
2025-03-04 16:09:55 +0900

    0을 포함한 양의 정수 입력하지 않았을 경우 메세지 출력

--- src/level1/Main.java
+++ src/level1/Main.java
@@ -7,13 +7,19 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("첫번째 숫자를 입력하세요 : ");
+        System.out.print("첫번째 숫자를 입력하세요 : ");
         int a = scanner.nextInt();
+         if (a < 0) {
+             System.out.println("0을 포함한 양의 정수로 입력하십시오.");
+         }
 
-        System.out.println("두번째 숫자를 입력하세요 : ");
+        System.out.print("두번째 숫자를 입력하세요 : ");
         int b = scanner.nextInt();
+        if (b < 0) {
+            System.out.println("0을 포함한 양의 정수로 입력하십시오.");
+        }
 
-        System.out.println("사칙연산 기호를 입력하세요(+, -, *, /) : ");
+        System.out.print("사칙연산 기호를 입력하세요(+, -, *, /) : ");
         char operation = scanner.next().charAt(0);
 
         int result = 0;
