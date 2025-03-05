62c9d94f7adf1879b96dcd3821676804ccd09795
강다연 <dysy9799@naver.com>
2025-03-05 22:28:37 +0900

    exit 입력 시 프로그램 종료

--- src/level2/App.java
+++ src/level2/App.java
@@ -8,36 +8,38 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
+        String str;
 
-        while (true){
-            while (true) {
-                System.out.println("첫번째 숫자를 입력하세요 : ");
-                int a = scanner.nextInt();
-                while (a < 0) {
-                    System.out.println("0을 포함한 양의 정수로 입력하십시오.");
-                    System.out.print("첫번째 숫자를 입력하세요 : ");
-                    a = scanner.nextInt();
-                }
-
-                System.out.println("두번째 숫자를 입력하세요 : ");
-                int b = scanner.nextInt();
-                while (b < 0) {
-                    System.out.println("0을 포함한 양의 정수로 입력하십시오.");
-                    System.out.print("두번째 숫자를 입력하세요 : ");
-                    b = scanner.nextInt();
-                }
-
-                System.out.println("사칙연산 기호를 입력하세요(+, -, *, /) : ");
-                char operation = scanner.next().charAt(0);
-
-                if (b == 0 && operation == '/') {
-                    System.out.println("0으로 나눌 수 없습니다. 올바른 값을 입력하세요.");
-                    break;
-                }
-
-                int result = calculator.calc(a, b, operation);
-                System.out.println("값은 : " + result);
+        do {
+            System.out.print("첫번째 숫자를 입력하세요 : ");
+            int a = scanner.nextInt();
+            while (a < 0) {
+                System.out.println("0을 포함한 양의 정수로 입력하십시오.");
+                System.out.print("첫번째 숫자를 입력하세요 : ");
+                a = scanner.nextInt();
             }
-        }
+
+            System.out.print("두번째 숫자를 입력하세요 : ");
+            int b = scanner.nextInt();
+            while (b < 0) {
+                System.out.println("0을 포함한 양의 정수로 입력하십시오.");
+                System.out.print("두번째 숫자를 입력하세요 : ");
+                b = scanner.nextInt();
+            }
+
+            System.out.print("사칙연산 기호를 입력하세요(+, -, *, /) : ");
+            char operation = scanner.next().charAt(0);
+
+            if (b == 0 && operation == '/') {
+                System.out.println("0으로 나눌 수 없습니다. 올바른 값을 입력하세요.");
+            }
+            int result = calculator.calc(a, b, operation);
+            System.out.println("값은 : " + result);
+
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            str = scanner.next();
+        }while (!str.equals("exit"));
+        System.out.println("프로그램이 종료됩니다.");
     }
 }
