f53cfdf51c660cded680e3908dd5ddf9377a3033
강다연 <dysy9799@naver.com>
2025-03-05 21:14:10 +0900

    중복되는 list 제거

--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Calculator {
 
-    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<Integer> list;
 
     public Calculator(){
         this.list = new ArrayList<>();
@@ -14,7 +14,6 @@ public class Calculator {
 
         int result = 0;
 
-        // 나눗셈 예외 처리 (분모가 0이면 에러 메시지 출력 후 0 반환)
         if (operation == '/' && b == 0) {
             System.out.println("0으로 나눌 수 없습니다. 올바른 값을 입력하세요.");
             return 0;
