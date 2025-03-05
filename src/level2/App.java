a3529eb37d6e73884da255ca746f452a2384aedc
강다연 <dysy9799@naver.com>
2025-03-05 21:21:40 +0900

    사용자에게 입력받기

--- src/level2/App.java
+++ src/level2/App.java
@@ -9,5 +9,17 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
 
+        do {
+            System.out.println("첫번째 숫자를 입력하세요 : ");
+            int a = scanner.nextInt();
+
+            System.out.println("두번째 숫자를 입력하세요 : ");
+            int b = scanner.nextInt();
+
+            System.out.println("사칙연산 기호를 입력하세요(+, -, *, /) : ");
+            char operation = scanner.next().charAt(0);
+
+            calculator.calc(a,b,operation);
+        }while(true);
     }
 }
