e3a2d0fc5a3b00005604aa6214303bd63c766ec7
강다연 <dysy9799@naver.com>
2025-03-05 18:40:53 +0900

    calculator 객체 생성

--- src/level2/App.java
+++ src/level2/App.java
@@ -1,4 +1,13 @@
 package level2;
 
+import java.util.Scanner;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+    }
 }
