2d9e7bd20dfe1d239c6da869c5e38195a6045d92
강다연 <dysy9799@naver.com>
2025-03-05 18:38:37 +0900

    -사칙연산을 수행한 후 결과값을 반환하는 메서드 구현 -연산 결과를 저장하는 컬렉션 타입 필드 Arraylist 사용

--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -1,4 +1,43 @@
 package level2;
 
+import java.util.ArrayList;
+
 public class Calculator {
+
+    ArrayList<Integer> list = new ArrayList<>();
+
+    public Calculator(){
+        this.list = new ArrayList<>();
+    }
+
+    public int calc(int a, int b, char operation) {
+
+        int result = 0;
+
+        // 나눗셈 예외 처리 (분모가 0이면 에러 메시지 출력 후 0 반환)
+        if (operation == '/' && b == 0) {
+            System.out.println("0으로 나눌 수 없습니다. 올바른 값을 입력하세요.");
+            return 0;
+        }
+
+        switch (operation) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            default:
+                System.out.println("잘못된 연산자입니다. (+, -, *, /)만 입력하세요.");
+                return 0;
+        }
+        list.add(result);
+        return result;
+    }
 }
